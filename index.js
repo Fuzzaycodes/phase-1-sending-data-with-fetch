e2405d8c14269d37a5ad7f26be06a442843327cd
Fuzzay <samomwerif2@gmail.com>
2024-07-21 15:43:40 +0300

    fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+   
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({ name, email }),
+    })
+      .then(response => response.json()) 
+      .then(data => {
+        
+        const newIdElement = document.createElement('p');
+        newIdElement.textContent = `New ID: ${data.id}`;
+        document.body.appendChild(newIdElement);
+      })
+      .catch(error => {
+        
+        const errorMessageElement = document.createElement('p');
+        errorMessageElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorMessageElement);
+      });
+  }
+  
+  
+  submitData('sam', 'samomwerif2@gmail.com');
+  
